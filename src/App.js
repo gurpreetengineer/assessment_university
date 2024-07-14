f2acf4923308503a229ae2c3c63499fbcca0e902
gurpreetengineer <gb.gurpreetbamrah@gmail.com>
2024-07-14 17:10:05 +0530

    re commiting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ListingPage from './views/ListingPage';
+import DetailsPage from './views/DetailsPage';
+import { saveToLocalStorage } from './utils/localStorage';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const handleSelectItem = (item) => {
+    setSelectedItem(item);
+    saveToLocalStorage(process.env.REACT_APP_STORAGE_KEY_TWO, item)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<ListingPage onSelectItem={handleSelectItem} />} />
+        <Route path="/details" element={<DetailsPage item={selectedItem} />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
